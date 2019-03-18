2312573cc74bb6cbb2cd9e3bc5af0ce988137458
EeliH <4096394+EeliH@users.noreply.github.com>
2019-03-18 16:56:03 +0200

    more frontend stuff

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,6 +3,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+var moment = require('moment');
+
 import './assets/stylesheets/style.css';
 
 const baseURL = process.env.ENDPOINT;
@@ -12,50 +14,61 @@ const baseURL = process.env.ENDPOINT;
 const getGreetingFromBackend = async () => {
   try {
     const url = `${baseURL}/api/temperature`;
-    console.log("Getting greeting from "+url);
+    console.log("Getting data from " + url);
     const response = await fetch(url);
     return response.json();
   } catch (error) {
     console.error(error);
   }
-  return { greeting :"Could not get greeting from backend"};
+  return {greetings: "Greetings, traveller!"};
 };
 
-
 const InfoBox = (props) => (
-  <div>
-    <p>{props.temp}°C</p>
-    <p>{props.hum}%</p>
-  </div>
+  <table>
+    <tbody>
+      <tr>
+        <td>Timestamp</td>
+        <td>Temperature</td>
+        <td>Humidity</td>
+      </tr>
+      {props.data.map(entry => 
+      <tr key={entry.id}>
+        <td>
+          {moment(entry.timestamp).fromNow()}
+        </td>
+        <td>
+          {entry.temperature}
+        </td>
+        <td>
+          {entry.humidity}
+        </td>
+      </tr>
+      )}
+    </tbody>
+  </table>
 );
-
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      temp: null,
-      hum: null,
+      data: [],
     };
   }
 
   async componentWillMount() {
     const response = await getGreetingFromBackend();
     console.log(response);
-    const latestEntry = response["results"].slice(-1)[0];
-    this.setState(
-      {
-        temp: latestEntry["temperature"], 
-        hum: latestEntry["humidity"]
-      }
-    );
+    const entries = response["results"];
+    console.log(entries);
+
+    this.setState({data: entries});
   }
 
   render() {
-
     return (
-      <InfoBox temp={this.state.temp} hum={this.state.hum} />
+      <InfoBox data={this.state.data} />
     );
   }
 }
